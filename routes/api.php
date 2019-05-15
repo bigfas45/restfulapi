e21655f05e36e8486790cd28ebd523984142d4c6
vagrant <vagrant@restfulapi>
2019-05-15 14:16:32 +0000

    First commit

---
+++ routes/api.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Http\Request;
+;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Buyers
+
+Route::resource('buyers', 'Buyer\BuyerController',['only' =>['index', 'show']]);
+
+// categories
+
+Route::resource('categories', 'Category\CategoryController',['except' =>['create', 'edit']]);
+
+// Products
+
+Route::resource('Products', 'Product\ProductController',['only' =>['index', 'show']]);
+
+// Sellers
+
+Route::resource('sellers', 'Seller\SellerController',['only' =>['index', 'show']]);
+
+// Transactions
+
+Route::resource('transactions', 'Transaction\TransactionController',['only' =>['index', 'show']]);
+
+// Users
+
+Route::resource('user', 'User\UserController',['except' =>['create', 'edit']]);
+
+
